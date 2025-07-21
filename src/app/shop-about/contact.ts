6e55d8c39fcd26601b88b0079db029fc5944a371
Milagros Lopez Vilaclara <lopezvmilagros@gmail.com>
2025-07-20 21:29:48 -0300

    contactos

---
+++ src/app/shop-about/contact.ts
@@ -0,0 +1,5 @@
+export interface Contact {
+  name: string;
+  tel: number;
+  email: string;
+}
--- src/app/shop-about/shop-about.ts
+++ src/app/shop-about/shop-about.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './contact';
 
 @Component({
   selector: 'app-shop-about',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './shop-about.scss'
 })
 export class ShopAbout {
+  contacts: Contact[] = [ //Un arreglo, una colección del tipo Hamster
+      //MOCK de Objeto literal
+      {
+        name: "Clara Franco",
+        tel: 2494571250,
+        email: "francoclara@gmail.com",
+      },
   
+      {
+        name: "Milagros Lopez",
+        tel: 2494655633,
+        email: "lopezvmila@gmail.com",
+      },
+  
+      {
+        name: "Victoria Franceschetti",
+        tel: 2494900349,
+        email: "vickyfrances@gmail.com",
+      },
+  
+  ];
 }
